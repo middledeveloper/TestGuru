082194d14f3e9c92fe6a91f27ff3786fb024c2bb
middledeveloper <nikolay.tikhvin@gmail.com>
2020-01-14 00:41:21 +0300

    Task03.02

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.by_category_title_repo(category_title)
+    id_value = Category.where(title: category_title).first
+    repo = Test.where(category_id: id_value).order('id DESC').pluck(:title)
+    # repo.each { |test| puts test }
+  end
 end
