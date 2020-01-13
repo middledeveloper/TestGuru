7ad82b74c69a2f372e5ddb70e5c53540bf3fb8b5
middledeveloper <nikolay.tikhvin@gmail.com>
2020-01-13 23:38:37 +0300

    Task03.01

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(title: 'Backend')
+Category.create(title: 'Frontend')
+Category.create(title: 'Others')
+
+Test.create(title: 'Ruby', level: 1, category_id: 1)
+Test.create(title: 'Rails', level: 2, category_id: 1)
+Test.create(title: 'C#', level: 0, category_id: 1)
+Test.create(title: '.Net Core', level: 1, category_id: 1)
+Test.create(title: 'JavaScript', level: 0, category_id: 2)
+Test.create(title: 'React', level: 1, category_id: 2)
+Test.create(title: 'Angular', level: 1, category_id: 2)
+
+Question.create(text: 'What is Ruby?', number: 1, test_id: 1)
+Question.create(text: 'What means == ?', number: 2, test_id: 1)
+Question.create(text: 'What is Rails?', number: 1, test_id: 2)
+Question.create(text: 'What means to_s ?', number: 2, test_id: 2)
+Question.create(text: 'What is C#?', number: 1, test_id: 3)
+Question.create(text: 'What is ternary operator', number: 2, test_id: 3)
+
+Answer.create(text: 'Fish', correct: false, question_id: 1)
+Answer.create(text: 'Fruit', correct: false, question_id: 1)
+Answer.create(text: 'Programming language', correct: true, question_id: 1)
+
+Answer.create(text: 'Oil', correct: false, question_id: 2)
+Answer.create(text: 'Animal', correct: false, question_id: 2)
+Answer.create(text: 'Ruby on Rails framework', correct: true, question_id: 2)
+
+Answer.create(text: 'Machine', correct: false, question_id: 3)
+Answer.create(text: 'Weapon', correct: false, question_id: 3)
+Answer.create(text: 'Programming language', correct: true, question_id: 3)
+
+User.create(name: 'Rob', login: 'rob', role: 1)
+User.create(name: 'Bob', login: 'bob', role: 1)
+User.create(name: 'John', login: 'john01', role: 1)
+User.create(name: 'Admin', login: 'admin', role: 0)
