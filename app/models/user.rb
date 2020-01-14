0a4f1c11dad86f75bb0472dd468f90824d61912c
middledeveloper <nikolay.tikhvin@gmail.com>
2020-01-15 01:10:23 +0300

    Task03.03

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  def history(test_level)
+    tests_ids = UsersHistory.where(user_id: id).pluck(:test_id)
+    repo = Test.where('id IN (?) AND level = ?', tests_ids, test_level)
+  end
 end
