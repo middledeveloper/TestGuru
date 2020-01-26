f22eacae8b11056d74c70845cd0453bb634753f1
middledeveloper <nikolay.tikhvin@gmail.com>
2020-01-26 21:36:01 +0300

    Task04.04

--- db/migrate/20200124221017_add_author_to_tests.rb
+++ db/migrate/20200124221017_add_author_to_tests.rb
@@ -2,7 +2,7 @@
 
 class AddAuthorToTests < ActiveRecord::Migration[6.0]
   def change
-    add_column :tests, :author_id, :integer, dependent: :nullify
+    add_column :tests, :author_id, :integer
     add_index :tests, :author_id
   end
 end
