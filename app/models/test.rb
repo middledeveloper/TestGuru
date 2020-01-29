a5ba175735ccc60fb3fabdbdcc2cce6653758293
middledeveloper <nikolay.tikhvin@gmail.com>
2020-01-30 00:44:42 +0300

    Task05.01

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,11 +8,28 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
-  def self.by_category_title_repo(category_title)
-    Test
-      .joins(:category)
-      .where(categories: { title: category_title })
-      .order('tests.id DESC')
-      .pluck(:title)
-  end
+  scope :simple, -> { where(level: [0..1]) }
+  scope :middle, -> { where(level: [2..4]) }
+  scope :hard, -> { where(level: [5..]) } # or [5..Float::INFINITY]
+
+  scope :by_category_title_repo, ->(category_title) {
+                                  joins(:category)
+                                  .where(categories: { title: category_title })
+                                  .order(id: :desc)
+                                  .pluck(:title)
+                                }
+
+  validates :title, presence: true
+  validates :level, numericality: { only_integer: true, greater_than: 0 }
+  # unique 'title + level' pair values
+  validates :title, uniqueness: {
+    scope: :level, message: 'We already have this test in Db' }
+
+  #def self.by_category_title_repo(category_title)
+  #    Test
+  #      .joins(:category)
+  #      .where(categories: { title: category_title })
+  #      .order('tests.id DESC')
+  #      .pluck(:title)
+  #  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_085328) do
+ActiveRecord::Schema.define(version: 2020_01_29_200424) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_01_26_085328) do
     t.integer "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   create_table "users_histories", force: :cascade do |t|
