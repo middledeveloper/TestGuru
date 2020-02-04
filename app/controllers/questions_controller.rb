b9c546139f43839ef443f40839586d865f289d82
middledeveloper <nikolay.tikhvin@gmail.com>
2020-02-04 23:59:19 +0300

    Task06.02

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,29 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
+  rescue_from RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    render plain: @test.questions.inspect
+  end
+
+  def destroy
+    @question.destroy
+    render plain: "Question id:#{params[:id]} deleted!"
+  end
+
+  private
+
+  def rescue_with_question_not_found
+    render plain: "Question id:#{params[:id]} not exist!",
+           status: 404
+  end
+
+  def define_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def define_question
+    @question = Question.find(params[:id])
+  end
 end
