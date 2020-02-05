1c89ed6506f54ea9120017d50593ee092b4fbc09
middledeveloper <nikolay.tikhvin@gmail.com>
2020-02-06 01:08:58 +0300

    Task06.03

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,24 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  rescue_from RecordNotFound, with: :rescue_with_question_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  before_action :define_test, only: %i[index create]
+  before_action :define_question, only: %i[show destroy]
 
   def index
     render plain: @test.questions.inspect
+  end
+
+  def new; end
+
+  def create
+    question = @test.questions.create(question_params)
+    render plain: question.inspect
+  end
+
+  def show
+    render plain: @question.inspect
   end
 
   def destroy
@@ -25,5 +39,9 @@ class QuestionsController < ApplicationController
 
   def define_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:test_id, :text)
   end
 end
